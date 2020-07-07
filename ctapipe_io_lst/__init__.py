Fixed issue by transposing array of single-p.e. pulse shapes Return quantities with units directly from read_pulse_shapes

ctapipe_io_lst/__init__.py
@@ -50,13 +50,28 @@ def load_camera_geometry(version=4):
 
 def read_pulse_shapes():
 
+    '''
+    Reads in the data on the pulse shapes and readout speed, from an external
+    file
+
+    Returns:
+
+    time between samples in the actual DAQ (ns, astropy quantity)
+    time between samples in the returned single-p.e pulse shape (ns, astropy
+    quantity)
+    Single-p.e. pulse shapes, ndarray of shape (2, 1640)
+
+    '''
+
     infilename = resource_filename('ctapipe_io_lst',
                                    'resources/oversampled_pulse_LST_8dynode_pix6_20200204.dat')
     data = np.genfromtxt(infilename, dtype='float', comments='#')
-    daq_time_per_sample = data[0, 0] # ns
-    pulse_shape_time_step = data[0, 1] # ns
+    daq_time_per_sample = data[0, 0] * u.ns
+    pulse_shape_time_step = data[0, 1] * u.ns
 
-    return daq_time_per_sample, pulse_shape_time_step, data[1:,]
+    # Note we have to transpose the pulse shapes array to provide what ctapipe
+    # expects:
+    return daq_time_per_sample, pulse_shape_time_step, data[1:,].T
 
 
 class LSTEventSource(EventSource):