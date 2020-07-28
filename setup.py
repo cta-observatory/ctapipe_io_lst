0f1b45362d0a8fd220261980d9bd62dd8071863e
Jose Enrique Ruiz <jer@iaa.es>
2020-07-28 17:54:37 +0200

    remove duplicated version.py

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup, find_packages
 from os import path
-from version import get_version, update_release_version
+import sys
+
+# add ctapipe_io_lst folder to path so to access version.py
+sys.path.insert(0, path.join(path.dirname(__file__), 'ctapipe_io_lst'))
+from version import get_version, update_release_version  # noqa
+
+update_release_version()
+version = get_version()
 
 # read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-
-
-update_release_version()
-version = get_version()
 
 setup(
     name='ctapipe_io_lst',
@@ -22,7 +25,6 @@ setup(
         'astropy',
         'ctapipe',
         'protozfits @ https://github.com/cta-sst-1m/protozfitsreader/archive/v1.4.2.tar.gz',
-
     ],
     package_data={
         'ctapipe_io_lst': ['resources/*'],
