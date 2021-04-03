028a0ab37684c4196379ab6941bbb4f5c7f81832
SeiyaNozaki <nozaki.seiya.1211@gmail.com>
2021-04-03 14:41:48 +0000

    integrate a function to subtract adc with one to find spike cells

--- ctapipe_io_lst/calibration.py
+++ ctapipe_io_lst/calibration.py
@@ -205,8 +205,7 @@ class LSTR0Corrections(TelescopeComponent):
             self.subtract_pedestal(event, tel_id)
             self.time_lapse_corr(event, tel_id)
             #self.interpolate_spikes(event, tel_id)
-            self.find_spike_position(event, tel_id)
-            self.subtract_spikes(event, tel_id)
+            self.spike_corr(event, tel_id)
 
 
             # remove samples at beginning / end of waveform
@@ -582,52 +581,48 @@ class LSTR0Corrections(TelescopeComponent):
                             interpolate_spike_A(waveform, gain, spike_A_position, pix)
         return waveform
 
-    def find_spike_position(self, event, tel_id):
+    def spike_corr(self, event, tel_id):
         """
-        Find Spike cell positions in a readout window
+        Subtract corresponding spike ADC counts
 
         Parameters
         ---------
         event : `ctapipe` event-container
         tel_id : id of the telescope
         """
-        run_id = event.lst.tel[tel_id].svc.configuration_id
+
+        lst = event.lst.tel[tel_id]
+
+        # If R1 container exists, update it inplace
+        if isinstance(event.r1.tel[tel_id].waveform, np.ndarray):
+            container = event.r1.tel[tel_id]
+        else:
+            # Modify R0 container. This is to create pedestal files.
+            container = event.r0.tel[tel_id]
+
+        waveform = container.waveform.copy()
+
         
         # We have 2 functions: one for data from 2018/10/10 to 2019/11/04 and
         # one for data from 2019/11/05 (from Run 1574) after update firmware.
         # The old readout (before 2019/11/05) is shifted by 1 cell.
+        run_id = event.lst.tel[tel_id].svc.configuration_id        
         if run_id > LAST_RUN_WITH_OLD_FIRMWARE:
-            find_spike_position = find_spike_position_jit
+            subtract_spikes = subtract_spikes_jit
         else:
-            find_spike_position  = find_spike_position_jit_data_from_20181010_to_20191104
+            subtract_spikes = subtract_spikes_jit_data_from_20181010_to_20191104
 
-        find_spike_position(
+        # corresponding spike adc counts are hard-coded for now. Those values will be loaded from a pedestal file.
+        spike_adc = np.array([47, 54, 8])
+            
+        subtract_spikes(
+            waveform,
             self.first_cap[tel_id],
             self.first_cap_old[tel_id],
             self.spike_flag[tel_id],
+            spike_adc, 
         )
-
-    def subtract_spikes(self, event, tel_id):
-        """
-        Subtract ADC counts corresponding spikes
-        Fill the R1 container.
-        Parameters
-        ----------
-        event : `ctapipe` event-container
-        tel_id : id of the telescope
-        """
-
-        waveform = event.r1.tel[tel_id].waveform.copy()
-        spike_adc = np.array([47, 54, 8], dtype=np.float32)
-
-        subtract_spikes_jit(
-            waveform,
-            self.spike_flag[tel_id],
-            spike_adc,
-        )
-        event.r1.tel[tel_id].waveform = waveform
-
-
+        container.waveform = waveform
 
 @njit(cache=True)
 def subtract_pedestal_jit(
@@ -774,10 +769,12 @@ def do_time_lapse_corr_data_from_20181010_to_20191104(
                             last_readout_time[gain, pixel_id, capacitor % N_CAPACITORS_PIXEL] = time_now
 
 @njit(cache=True)
-def find_spike_position_jit(
+def subtract_spikes_jit(
+    waveform,
     first_capacitor,
     first_capacitor_old,
     spike_flag,
+    spike_adc,
 ):
     """
     Numba function for finding spike positions in readout windows
@@ -803,22 +800,27 @@ def find_spike_position_jit(
 
                     if (spike_pos - current_fc) % N_CAPACITORS_CHANNEL < N_SAMPLES:
                         spike_flag[gain, pixel, k, (spike_pos - current_fc) % N_CAPACITORS_CHANNEL] = True
-
+                        waveform[gain, pixel,(spike_pos - current_fc) % N_CAPACITORS_CHANNEL] -= spike_adc[k]
+                        
                     spike_pos = (N_CAPACITORS_CHANNEL - 2 + k - last_lc) % N_CAPACITORS_CHANNEL
 
                     if (spike_pos - current_fc) % N_CAPACITORS_CHANNEL < N_SAMPLES:
                         spike_flag[gain, pixel, k, (spike_pos - current_fc) % N_CAPACITORS_CHANNEL] = True   
-
+                        waveform[gain, pixel,(spike_pos - current_fc) % N_CAPACITORS_CHANNEL] -= spike_adc[k]
+                        
 @njit(cache=True)
-def find_spike_position_jit_data_from_20181010_to_20191104(
+def subtract_spikes_jit_data_from_20181010_to_20191104(
+    waveform,
     first_capacitor,
     first_capacitor_old,
     spike_flag,
+    spike_adc,
 ):
     """
     Numba function for finding spike positions in readout windows
     """
     LAST_IN_FIRST_HALF = N_CAPACITORS_CHANNEL // 2 - 1
+    
 
     for gain in range(N_GAINS):
         for pixel in range(N_PIXELS):
@@ -834,31 +836,19 @@ def find_spike_position_jit_data_from_20181010_to_20191104(
             if last_lc % 2 == 0 and last_lc<= LAST_IN_FIRST_HALF:
                 
                 for k in range(3):
-                    spike_pos = (last_lc + k - 1) % N_CAPACITORS_CHANNEL
+                    spike_pos = (last_lc -1 + k) % N_CAPACITORS_CHANNEL
 
                     if (spike_pos - current_fc) % N_CAPACITORS_CHANNEL < N_SAMPLES:
                         spike_flag[gain, pixel, k, (spike_pos - current_fc) % N_CAPACITORS_CHANNEL] = True
+                        waveform[gain, pixel,(spike_pos - current_fc) % N_CAPACITORS_CHANNEL] -= spike_adc[k]
 
                     spike_pos = (N_CAPACITORS_CHANNEL - 3 + k - last_lc) % N_CAPACITORS_CHANNEL
 
                     if (spike_pos - current_fc) % N_CAPACITORS_CHANNEL < N_SAMPLES:
                         spike_flag[gain, pixel, k, (spike_pos - current_fc) % N_CAPACITORS_CHANNEL] = True   
+                        waveform[gain, pixel,(spike_pos - current_fc) % N_CAPACITORS_CHANNEL] -= spike_adc[k]
 
-@njit(cache=True)   
-def subtract_spikes_jit(                                                                                                                    
-    waveform,                                                                                                                                               
-    spike_flag,                                                                                                                                      
-    spike_adc,
-):
-    """
-    Numba function for subtraction of ADC counts corresponding spikes
-    """
-    for gain in range(N_GAINS):
-        for pixel in range(N_PIXELS):
-            for i in range(3):
-                waveform[gain, pixel][spike_flag[gain, pixel, i]] -= spike_adc[i]
-        
-                
+
 @njit(cache=True)
 def ped_time(timediff):
     """
