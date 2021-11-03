f74749f1ce774263695ab335a799938334fa9891
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-11-03 15:21:23 +0100

    Allow ctapipe 0.11 and test using it

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     use_scm_version={"write_to": os.path.join("ctapipe_io_lst", "_version.py")},
     install_requires=[
         'astropy~=4.2',
-        'ctapipe~=0.10.0',
+        'ctapipe>=0.10.5,<0.12',
         'protozfits~=2.0',
         'setuptools_scm',
         'traitlets~=5.0.5', # remove when upgrading to ctapipe > 0.11
