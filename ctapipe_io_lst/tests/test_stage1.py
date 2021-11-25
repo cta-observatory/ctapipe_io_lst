4a796d4cbb9172a5805481b0632f1c0e0b2241f7
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-11-25 14:43:30 +0100

    Add test for ff option

--- ctapipe_io_lst/tests/test_stage1.py
+++ ctapipe_io_lst/tests/test_stage1.py
@@ -87,3 +87,76 @@ def test_stage1(tmpdir):
     assert event_type_counts[EventType.FLATFIELD.value] == 1
     assert event_type_counts[EventType.SKY_PEDESTAL.value] == 1
     assert event_type_counts[EventType.SUBARRAY.value] == 198
+
+
+def test_no_ff_tagging(tmpdir):
+    """Test the ctapipe stage1 tool can read in LST real data using the event source"""
+    from ctapipe.tools.stage1 import Stage1Tool
+    from ctapipe.core.tool import run_tool
+
+    tmpdir = Path(tmpdir)
+    config_path = tmpdir / 'config.json'
+
+    config = {
+        'LSTEventSource': {
+            "use_flatfield_heuristic": False,
+            'LSTR0Corrections': {
+                'drs4_pedestal_path': str(test_drs4_pedestal_path),
+                'drs4_time_calibration_path': str(test_time_calib_path),
+                'calibration_path': str(test_calib_path),
+            },
+            'PointingSource': {
+                'drive_report_path': str(test_drive_report)
+            },
+            'EventTimeCalculator': {
+                'run_summary_path': str(test_run_summary),
+            },
+        },
+        "CameraCalibrator": {
+            "image_extractor_type": "LocalPeakWindowSum",
+            "LocalPeakWindowSum": {
+                "window_shift": 4,
+                "window_width": 8,
+                "apply_integration_correction": False,
+            }
+        },
+        "TailcutsImageCleaner": {
+            "picture_threshold_pe": 6,
+            "boundary_threshold_pe": 3,
+            "keep_isolated_pixels": False,
+            "min_picture_neighbors": 1,
+        }
+    }
+    with config_path.open('w') as f:
+        json.dump(config, f)
+
+    tool = Stage1Tool()
+    output = tmpdir / "test_dl1.h5"
+
+    ret = run_tool(tool, argv=[
+        f'--input={test_r0_path}',
+        f'--output={output}',
+        f'--config={config_path}',
+    ])
+    assert ret == 0
+
+    # test our custom default works
+    assert tool.event_source.r0_r1_calibrator.gain_selector.threshold == 3500
+
+    parameters = read_table(output, '/dl1/event/telescope/parameters/tel_001')
+    assert len(parameters) == 200
+
+    trigger = read_table(output, '/dl1/event/subarray/trigger')
+
+    # test regression of event time calculation
+    first_event_time = Time(59101.95035244, format='mjd', scale='tai')
+    assert np.all((trigger['time'] - first_event_time).to_value(u.s) < 10)
+
+    event_type_counts = np.bincount(trigger['event_type'])
+
+    # one pedestal and flat field expected each, rest should be physics data
+    # without ff heuristic, the ff event will have type SUBARRAY
+    assert event_type_counts.sum() == 200
+    assert event_type_counts[EventType.FLATFIELD.value] == 0
+    assert event_type_counts[EventType.SKY_PEDESTAL.value] == 1
+    assert event_type_counts[EventType.SUBARRAY.value] == 199
