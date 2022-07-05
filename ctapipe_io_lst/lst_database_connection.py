836e76b1c53d685e6d94a0a16a70a7b73ae48494
GregoireUhlrich <gregoire.uhlrich@cern.ch>
2022-07-05 12:25:15 +0000

    Doc strings lst connection

--- ctapipe_io_lst/lst_database_connection.py
+++ ctapipe_io_lst/lst_database_connection.py
@@ -4,13 +4,14 @@ import numpy as np
 import psycopg2 as pg
 
 def _adapt_array(text):
+    """ Adapt a numpy array and return a Postgres binary object. """
     out = io.BytesIO()
     np.save(out, text)
     out.seek(0)
     return pg.Binary(zlib.compress(out.read()))
 
-# converts from<Plug>PeepOpenostgres to<Plug>PeepOpenython
 def _typecast_array(value, cur):
+    """ Convert back a Postgres binary object into a numpy array. """
     if value is None:
         return None
 
@@ -25,6 +26,7 @@ pg.extensions.register_type(t_array)
 
 
 def get_waveform_calibration_fields():
+    """ Fields of the waveform calibration table.  """
     return [
         'time',
         'time_min',
@@ -38,6 +40,7 @@ def get_waveform_calibration_fields():
 
 
 def get_flatfield_calibration_fields():
+    """ Fields of flatfield calibration table.  """
     return [
         'n_events',
         'sample_time',
@@ -60,6 +63,7 @@ def get_flatfield_calibration_fields():
 
 
 def get_pedestal_calibration_fields():
+    """ Fields of pedestal calibration table.  """
     return [
         'n_events',
         'sample_time',
@@ -74,6 +78,7 @@ def get_pedestal_calibration_fields():
 
 
 def get_pixel_status_fields():
+    """ Fields of pixel status calibration table.  """
     return [
         'hardware_failing_pixels',
         'pedestal_failing_pixels',
@@ -84,18 +89,26 @@ def get_pixel_status_fields():
 class LSTDatabaseConnection:
 
     def __init__(self, user, database, autocommit=False):
+        """ Initialise the instance. The connection is not open.  """
         self.autocommit = autocommit
         self.user = user
         self.database = database
 
     def load_calibration_data(self, run):
+        """
+        Load the calibration data corresponding to a given run.
+        (waveform, flatfield, pedestal and status)
+        """
         waveform_fields = get_waveform_calibration_fields()
         self.cursor.execute(
             "SELECT " + ','.join(waveform_fields)
             + " FROM waveform_calibration_pro where run=%s",
             (run,))
         res_calib = self.cursor.fetchone()
-        calib = {field_name: res_calib[i] for i, field_name in enumerate(waveform_fields)}
+        calib = {
+            field_name: res_calib[i]
+            for i, field_name in enumerate(waveform_fields)
+        }
 
         flatfield_fields = get_flatfield_calibration_fields()
         self.cursor.execute(
@@ -103,7 +116,10 @@ class LSTDatabaseConnection:
             + " FROM flatfield_calibration_pro where run=%s",
             (run,))
         res_flatfield = self.cursor.fetchone()
-        flatfield = {field_name: res_flatfield[i] for i, field_name in enumerate(flatfield_fields)}
+        flatfield = {
+            field_name: res_flatfield[i]
+            for i, field_name in enumerate(flatfield_fields)
+        }
 
         pedestal_fields = get_pedestal_calibration_fields()
         self.cursor.execute(
@@ -111,7 +127,10 @@ class LSTDatabaseConnection:
             + " FROM pedestal_calibration_pro where run=%s",
             (run,))
         res_pedestal = self.cursor.fetchone()
-        pedestal = {field_name: res_pedestal[i] for i, field_name in enumerate(pedestal_fields)}
+        pedestal = {
+            field_name: res_pedestal[i]
+            for i, field_name in enumerate(pedestal_fields)
+        }
 
         pixel_status_fields = get_pixel_status_fields()
         self.cursor.execute(
@@ -119,7 +138,10 @@ class LSTDatabaseConnection:
             + " FROM pixel_status_pro where run=%s",
             (run,))
         res_pixel = self.cursor.fetchone()
-        pixel = {field_name: res_pixel[i] for i, field_name in enumerate(pixel_status_fields)}
+        pixel = {
+            field_name: res_pixel[i]
+            for i, field_name in enumerate(pixel_status_fields)
+        }
 
         return {
             'calibration': calib,
@@ -129,24 +151,44 @@ class LSTDatabaseConnection:
         }
 
     def load_drs4_pedestal_data(self, run):
-        self.cursor.execute("SELECT baseline_mean FROM drs4_baseline_pro where run=%s", (run,))
+        """
+        Load the drs4 pedestal data corresponding to a given run.
+        """
+        self.cursor.execute(
+            "SELECT baseline_mean FROM drs4_baseline_pro where run=%s",
+            (run,)
+        )
         return self.cursor.fetchone()[0]
 
     def load_drs4_spike_height_data(self, run):
-        self.cursor.execute("SELECT spike_height FROM drs4_baseline_pro where run=%s", (run,))
+        """
+        Load the drs4 spike data corresponding to a given run.
+        """
+        self.cursor.execute(
+            "SELECT spike_height FROM drs4_baseline_pro where run=%s",
+            (run,)
+        )
         return self.cursor.fetchone()[0]
 
     def load_drs4_time_calibration_data(self, run):
-        self.cursor.execute("SELECT fan, fbn FROM \"drs4_time_sampling_from_FF_pro\" where run=%s", (run,))
+        """
+        Load the drs4 time calibration data corresponding to a given run.
+        """
+        self.cursor.execute(
+            "SELECT fan, fbn FROM \"drs4_time_sampling_from_FF_pro\" where run=%s",
+            (run,)
+        )
         return self.cursor.fetchone()
 
     def open(self):
+        """ Open the connection to the database. """
         print(f'Connection to db {self.database!r}, user is {self.user!r}')
         self.connection = pg.connect(f'dbname={self.database} user={self.user}')
         self.cursor = self.connection.cursor()
         return self
 
     def close(self):
+        """ Close the connection to the database. """
         if self.connection and self.autocommit:
             self.connection.commit()
         if self.cursor:
@@ -155,9 +197,11 @@ class LSTDatabaseConnection:
             self.connection.close()
 
     def __enter__(self):
+        """ Enter a context, open the connection. """
         self.open()
         return self
 
     def __exit__(self, *args):
+        """ Exit a context, close the connection. """
         self.close()
 
