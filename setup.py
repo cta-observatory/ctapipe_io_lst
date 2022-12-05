c0322fc9169782dd95308267f4ad342a4ea30b9e
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2022-12-05 12:41:35 +0100

    Update requirements for ctapipe 0.17

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ import os
 setup(
     packages=find_packages(exclude=["ctapipe_io_lst._dev_version"]),
     use_scm_version={"write_to": os.path.join("ctapipe_io_lst", "_version.py")},
+    python_requires=">=3.8",
     install_requires=[
-        'astropy~=4.2',
-        'ctapipe~=0.12',
+        'astropy~=5.0',
+        'ctapipe~=0.17.0',
         'protozfits~=2.0',
         'setuptools_scm',
-        'numpy>=1.20,<1.23'
+        'numpy>=1.20'
     ],
     package_data={
         'ctapipe_io_lst': ['resources/*'],
